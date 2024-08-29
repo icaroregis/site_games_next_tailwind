69ac0a334888c442eb9119f0d49262c1513c0176
icaroalmeida-somma <icaro.almeida@somma-it.com>
2024-08-28 23:45:15 -0300

    feat: navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+export const Navbar = () => {
+  return (
+    <nav className="flex flex-col gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 h-screen">
+      <div className="flex justify-center items-center">
+        <Image
+          src="/IMG_20240504_155217.svg"
+          alt="Logo"
+          className="max-w-full p-2"
+          width={200}
+          height={200}
+        />
+      </div>
+      <ul className="flex-1 my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Home</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Games</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Top 10</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
+      </ul>
+      <ul className="flex-3 my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">User</li>
+      </ul>
+    </nav>
+  );
+};
